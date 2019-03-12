13e16e1a066324f1fec54d28ad9892348791ff15
thomasbirien <thomasbirien@gmail.com>
2019-03-12 17:38:44 +0100

    setup for index controller

--- app/controllers/tribe_members_controller.rb
+++ app/controllers/tribe_members_controller.rb
@@ -1,6 +1,6 @@
 class TribeMembersController < ApplicationController
-
   def index
+    @tribe_members = TribeMember.all
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :tribe_members, only: [:index, :new, :create]
+  root 'tribe_members#index'
 end
