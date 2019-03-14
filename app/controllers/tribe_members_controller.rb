46a79a3582709a5eb69d58db7d61e5e593b4a4cb
thomasbirien <thomasbirien@gmail.com>
2019-03-15 00:37:36 +0100

    optimisation index view

--- app/controllers/tribe_members_controller.rb
+++ app/controllers/tribe_members_controller.rb
@@ -3,11 +3,12 @@ class TribeMembersController < ApplicationController
     if params[:name].present? || params[:surname] || params[:member_birthdate] || params[:ancestor_name]
       @search_result = TribeMember.search(params)
     else
-      @tribe_m = JSON.parse(File.read('public/tribe_members.json'))
-      respond_to do |format|
-        format.html
-        format.json { render json: @tribe_m }
-      end
+      # @tribe_m = JSON.parse(File.read('public/tribe_members.json'))
+      # respond_to do |format|
+      #   format.html
+      #   format.json { render json: @tribe_m }
+      # end
+      @tribe_m = TribeMember.all.paginate(:page => params[:page], :per_page => 40)
     end
     # @tribe_members = TribeMember.all
     # TribeMember.build_geojson
